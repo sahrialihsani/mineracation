22a65ea938edcf6732fe18c9d4d4ae3ec1f2019d
Sahrial Ihsani Ishak <67180039+sahrialihsani@users.noreply.github.com>
2022-02-16 00:46:08 +0700

    Update app_helper.py

--- app_helper.py
+++ app_helper.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def get_classes(file_path):
     # model = myModel(weights="imagenet")
-    model = load_model("C:/Users/Pc/Videos/VGG16_32_epochs_new.h5")
+    model = load_model("https://www.mediafire.com/file/4kqodbiqke74c2t/VGG16_32_epochs_new.h5")
     img = image.load_img(file_path, target_size=(240, 240))
     img=img_to_array(img)
     img=np.expand_dims(img,axis=0)
