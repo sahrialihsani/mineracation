97ddf47a1a3f95b816ab9a14ec14fc9c2c51a9c8
Sahrial Ihsani Ishak <67180039+sahrialihsani@users.noreply.github.com>
2022-02-15 21:49:02 +0700

    Update app_helper.py

--- app_helper.py
+++ app_helper.py
@@ -25,7 +25,7 @@ import numpy as np
 
 def get_classes(file_path):
     # model = myModel(weights="imagenet")
-    model = load_model("C:/Users/Pc/Videos/VGG16_32_epochs_new.h5")
+    model = load_model("https://drive.google.com/file/d/1EdV19ekRbnID25f3FXogLQ03h-HxW8yN/view?usp=sharing")
     img = image.load_img(file_path, target_size=(240, 240))
     img=img_to_array(img)
     img=np.expand_dims(img,axis=0)
